6613ae650e068e518abb8a644b85fac2085c139a
Douglas Silva <douglass.sousa@outlook.com.br>
2023-12-19 18:54:49 -0300

    feat: use new tyform validation

--- examples/angular/src/app/tyform-example.ts
+++ examples/angular/src/app/tyform-example.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { builder as tyform, string } from 'tyform'
-import { Form, FormBuilder } from 'packages/form/src/types';
+import { builder as tyform, string, boolean } from 'tyform'
+import type { Form, FormBuilder } from 'tyform/types';
 
 @Component({
   selector: 'tyform-example',
@@ -28,6 +28,8 @@ import { Form, FormBuilder } from 'packages/form/src/types';
       form label {
         display: flex;
         flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
       }
 
       form label input {
@@ -48,7 +50,21 @@ import { Form, FormBuilder } from 'packages/form/src/types';
     </style>
 
     <form>
-      <label>
+      <ng-container *ngFor="let key of stringKeys">
+        <label>
+          {{key}}:
+          <input [class.invalid]="form[key].invalid" [name]="key" [(ngModel)]="form[key].value" type="text"/>
+          <small>{{form[key].error.join(', ')}}</small>
+        </label>
+      </ng-container>
+      <ng-container *ngFor="let key of booleanKeys">
+        <label>
+          {{key}}
+          <input [name]="key" [(ngModel)]="form[key].value" type="checkbox" >
+          <small>{{form[key].error.join(', ')}}</small>
+        </label>
+      </ng-container>
+      <!-- <label>
         Name:
         <input [class.invalid]="form.Name.invalid" name="Name" [(ngModel)]="form.Name.value" type="text"/>
         <small>{{form.Name.error.join(', ')}}</small>
@@ -63,6 +79,10 @@ import { Form, FormBuilder } from 'packages/form/src/types';
         <input [class.invalid]="form.Email.invalid" name="Email" [(ngModel)]="form.Email.value" type="text"/>
         <small>{{form.Email.error.join(', ')}}</small>
       </label>
+      <label>
+        Active
+        <input type="checkbox" name="" id="">
+      </label> -->
       <button [disabled]="isFormInvalid" (click)="submit()">Submit</button>
     </form>
   `,
@@ -75,6 +95,8 @@ export class TyformExampleComponent implements OnInit {
 
   private buider!: FormBuilder<Contact>;
   public form!: Form<Contact>;
+  public stringKeys!: (keyof Contact)[];
+  public booleanKeys!: (keyof Contact)[];
 
   public get isFormInvalid() {
     return Object.values(this.form).reduce((acc, cur) => cur.invalid || acc, false)
@@ -86,16 +108,22 @@ export class TyformExampleComponent implements OnInit {
       Name: string()
         .required("Name is required"),
       Phone: string()
-        .validate(value => this.PHONE_RGX.test(value)).withMessage('Phone format is invalid'),
+        .validate((value) => this.PHONE_RGX.test(value)).withMessage('Phone format is invalid'),
       Email: string()
-        .validate(value => this.EMAIL_RGX.test(value)).withMessage('Email format is invalid')
+        .validate(value => this.EMAIL_RGX.test(value)).withMessage('Email format is invalid'),
+      Notification: boolean<Contact>()
+        .validate((value, target) =>  value && (!!target?.Email.value || !!target?.Phone.value ), ['Email', 'Phone'])
+        .withMessage("You must have an email or phone number to receive notifications")
     }).init({
       Name: 'João Miguel',
       Phone: '27998765432',
-      Email: 'jao.miguel@hotmail.com'
+      Email: 'jao.miguel@hotmail.com',
+      Notification: false
     })
 
     this.form = this.buider.build();
+    this.booleanKeys = Object.entries(this.form).filter(([_, value]) => typeof value.value === 'boolean').map(([key]) => key as keyof Contact)
+    this.stringKeys = Object.entries(this.form).filter(([_, value]) => typeof value.value === 'string').map(([key]) => key as keyof Contact)
   }
 
   submit() {
@@ -108,4 +136,5 @@ interface Contact {
   Name: string;
   Phone: string;
   Email: string;
+  Notification: boolean;
 }
